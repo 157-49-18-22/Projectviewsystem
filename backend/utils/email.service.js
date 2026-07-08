dd5e1f3df5d539355d1e59f51b349b16abe84372
Abhi <576abhishek.2020@gmail.com>
2026-07-08 21:30:07 +0530

    feat: implement email service using nodemailer for SMTP communication

--- backend/utils/email.service.js
+++ backend/utils/email.service.js
@@ -2,7 +2,11 @@ const nodemailer = require('nodemailer');
 require('dotenv').config();
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail', 
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
+    requireTLS: true,
+    family: 4,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
